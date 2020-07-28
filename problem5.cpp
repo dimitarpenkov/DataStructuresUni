3787068dba19b87ce7ed7c86f93d98199a6010a2
dimitar.penkov <dimitar.penkov@openpayd.com>
2020-07-28 13:34:51 +0300

    Solve merge conflicts

--- problem5.cpp
+++ problem5.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 using namespace std;
 int main() {
+    cout << "this is problem5 on subBranch";
     cout << "this is the problem on mainSorting branch";
     cout << "hello there";
 }
